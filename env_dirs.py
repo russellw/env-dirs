85494ede1270e5278e3d1e9ee0085c41471ff552
Russell Wallace <russell.wallace@gmail.com>
2021-06-18 10:54:10 +0100

    glob

--- env_dirs.py
+++ env_dirs.py
@@ -6,28 +6,32 @@ import re
 parser = argparse.ArgumentParser()
 parser.add_argument("filenames", nargs="*")
 args = parser.parse_args()
-filenames=args.filenames
+filenames = args.filenames
 
-direx=re.compile(r"^[C-Zc-z]:\\.+")
+direx = re.compile(r"^[C-Zc-z]:\\.+")
 
-rs=[]
-for k,v in os.environ.items():
-    if v.startswith(';'):
-        v=v[1:]
+rs = []
+for k, v in os.environ.items():
+    if v.startswith(";"):
+        v = v[1:]
     if not direx.match(v):
         continue
-    v=v.split(';')
-    r=[k]
+    v = v.split(";")
+    r = [k]
     for d in v:
         if not direx.match(d):
             continue
         if not filenames:
             r.append(d)
             continue
-    if len(r)>1:
+        for filename in filenames:
+            if list(glob.glob(d + "/" + filename)):
+                r.append(d)
+                break
+    if len(r) > 1:
         rs.append(r)
 
 for r in rs:
-    print(f'{r[0]:40} {r[1]}')
-    for  d in r[2:]:
+    print(f"{r[0]:40} {r[1]}")
+    for d in r[2:]:
         print(f"{'':40} {d}")
